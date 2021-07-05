f32d10776672224c25251d8a59c07ec168237920
Clément OUDOT <clement@oodo.net>
2021-07-05 18:57:03 +0200

    Force user and group if container internal user is root

--- build/centos8/lemonldap-ng/ansible/run-ct.sh
+++ build/centos8/lemonldap-ng/ansible/run-ct.sh
@@ -1,31 +1,29 @@
 #!/bin/sh
 set -e
 
-echo "fusioniam:x:$(id -u):$(id -G | cut -d' ' -f 2):,,,:${HOME}:/bin/bash" >> /etc/passwd
-echo "fusioniam:x:$(id -G | cut -d' ' -f 2):" >> /etc/group
-
-export FUSIONIAM_UID=$(id -u)
-export FUSIONIAM_GID=$(id -G | cut -d' ' -f 2)
-
 /bin/bash /run-playbook.sh /deploy.yaml
 
 if [ "$1" = "nginx" ]
-then
+    then
         ln -s /dev/stdout /var/log/nginx/access.log
         ln -s /dev/stdout /var/log/nginx/error.log
         ln -s /dev/stdout /var/log/nginx/manager-api.log
         ln -s /dev/stdout /var/log/nginx/manager.log
         ln -s /dev/stdout /var/log/nginx/portal.log
-	/usr/sbin/nginx -g 'daemon off;'
+        /usr/sbin/nginx -g 'daemon off;'
 elif [ "$1" = "llng-fastcgi-server" ]
     then
-      sed 's/ //g' /etc/default/llng-fastcgi-server > /tmp/llng-fastcgi-server
-      /bin/bash  -c 'source /tmp/llng-fastcgi-server && /usr/libexec/lemonldap-ng/sbin/llng-fastcgi-server --foreground'
+        sed 's/ //g' /etc/default/llng-fastcgi-server > /tmp/llng-fastcgi-server
+        if [ $(id -u) -eq 0 ]; then
+            /bin/bash  -c 'source /tmp/llng-fastcgi-server && /usr/libexec/lemonldap-ng/sbin/llng-fastcgi-server -u apache -g apache --foreground'
+        else
+            /bin/bash  -c 'source /tmp/llng-fastcgi-server && /usr/libexec/lemonldap-ng/sbin/llng-fastcgi-server --foreground'
+        fi
 elif [ "$1" = "purge-sessions" ]
     then
-      while : ;
-      do /usr/libexec/lemonldap-ng/bin/purgeCentralCache -d -f; sleep 600;
-      done;
+        while : ; do
+            /usr/libexec/lemonldap-ng/bin/purgeCentralCache -d -f; sleep 600;
+        done
 fi
 
 exit 0
